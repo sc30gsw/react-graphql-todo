fafd71004e2cbc70f27a197d00d31cc1d920b464
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-27 19:10:53 +0900

    ApolloProvider・Clientの設定を追加

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { Toaster } from 'react-hot-toast'
 
+const httpLink = createHttpLink({ uri: 'http://localhost:4000' })
+
+const authLink = setContext((_: unknown, { headers }) => {
+  const token = sessionStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
